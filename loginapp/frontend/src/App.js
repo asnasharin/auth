b5abbd6f453853648b5f9da2c9eae5c49a443f73
asna sharin <asnasherin1233@gmail.com>
2024-06-28 10:09:34 +0530

    add frontend and create branch

---
+++ loginapp/frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './index.css'
+
+const App = () => {
+  return (
+    <>
+    <div className="bg-red-400">
+    <h1 className="text-3xl font-bold  text-cyan-800 ">
+    Hello world!
+  </h1>
+  </div>
+  <h2>hello</h2>
+  </>
+  )
+}
+
+export default App
